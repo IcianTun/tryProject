8f544e056a3ccfee9a100c022659baf5df1a417d
eieieician <magician_TH@hotmail.com>
2019-10-24 23:49:47 +0700

    Add Level and Playermovement

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+
+    public Transform player;
+    public float smoothing = 5f;
+
+    Vector3 positionOffset;
+
+    // Use this for initialization
+    void Start()
+    {
+        positionOffset = transform.position - player.position;
+        
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 targetCamPos = player.position + positionOffset;
+        transform.position = Vector3.Lerp(transform.position, targetCamPos, smoothing * Time.deltaTime);
+        transform.rotation = Quaternion.Lerp(transform.rotation, player.rotation, smoothing * Time.deltaTime);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float movementSpeed = 4f;
+    public float rotationSpeed = 90f;
+    Rigidbody playerRigidbody;
+
+
+    void Awake()
+    {
+        playerRigidbody = GetComponent<Rigidbody>();
+    }
+
+    private void FixedUpdate()
+    {
+        float h = Input.GetAxisRaw("Horizontal");
+        float v = Input.GetAxisRaw("Vertical");
+        Move(h, v);
+        Turning(Input.GetKey(KeyCode.Q), Input.GetKey(KeyCode.E));
+    }
+
+    void Move(float h, float v)
+    {
+        Vector3 vertical = transform.forward;
+        Vector3 horizontal = transform.right;
+
+        Vector3 movement = (vertical * v + horizontal * h) * movementSpeed * Time.deltaTime;
+        playerRigidbody.MovePosition(transform.position + movement);
+        ////playerRigidbody.AddForce(movement - playerRigidbody.velocity, ForceMode.VelocityChange);
+    }
+
+    void Turning(bool isTurnLeft, bool isTurnRight)
+    {
+        if (isTurnRight&&!isTurnLeft)
+        {
+            Quaternion deltaRotation = Quaternion.Euler(0, rotationSpeed * Time.deltaTime, 0);
+            playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
+        }
+        else if (isTurnLeft&&!isTurnRight)
+        {
+            Quaternion deltaRotation = Quaternion.Euler(0, -rotationSpeed * Time.deltaTime, 0);
+            playerRigidbody.MoveRotation(playerRigidbody.rotation * deltaRotation);
+        }
+    }
+    
+}
