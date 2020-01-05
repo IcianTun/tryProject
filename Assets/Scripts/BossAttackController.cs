92477c80c0033c8f5d06b6b59d7de593243a1b80
Supasin Rungsittikul <31331704+IcianTun@users.noreply.github.com>
2020-01-06 02:14:47 +0700

    Tried add attack script to boss by script

---
+++ Assets/Scripts/BossAttackController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossAttackController : MonoBehaviour {
+
+    IAttack attack1;
+
+    public float attackRate = 1.0f;
+    public float delay = 5.0f;
+
+    // Use this for initialization
+    void Start () {
+
+        // This work
+        //attack1 = gameObject.AddComponent<Attack1>();
+
+        // This work
+        attack1 = (IAttack) gameObject.AddComponent(System.Type.GetType("Attack1"));
+
+        InvokeRepeating("PerformAttack", delay, attackRate);
+    }
+	
+
+	void PerformAttack () {
+        attack1.Perform();
+	}
+}
---
+++ Assets/Scripts/New Folder/Attack1.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Attack1 : MonoBehaviour, IAttack {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    
+    public void Perform ()
+    {
+        Debug.Log("hello from attack1");
+    }
+}
